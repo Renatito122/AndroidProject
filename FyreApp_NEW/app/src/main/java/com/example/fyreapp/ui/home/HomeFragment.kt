7d8a59e462ac38554541ae80521f2379e4b1c1ed
pfig420 <palexandre09@ua.pt>
2022-12-29 17:25:36 +0000

    Programa Display done

--- FyreApp_NEW/app/src/main/java/com/example/fyreapp/ui/home/HomeFragment.kt
+++ FyreApp_NEW/app/src/main/java/com/example/fyreapp/ui/home/HomeFragment.kt
@@ -29,8 +29,42 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
+        val textView2: TextView = binding.textHome2
+        val textView3: TextView = binding.textHome3
+        val textView4: TextView = binding.textHome4
+        val textView5: TextView = binding.textHome5
+        val textView6: TextView = binding.textHome6
+        val textView7: TextView = binding.textHome7
+        val textView8: TextView = binding.textHome8
+        val textView9: TextView = binding.textHome9
+        val textView10: TextView = binding.textHome10
+        val textView11: TextView = binding.textHome11
+        val textView12: TextView = binding.textHome12
+        val textView13: TextView = binding.textHome13
+        val textView14: TextView = binding.textHome14
+        val textView15: TextView = binding.textHome15
+        val textView16: TextView = binding.textHome16
+        val textView17: TextView = binding.textHome17
+        val textView18: TextView = binding.textHome18
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+            textView2.text = "Nov 11 - 22:30 - Cuca Roseta"
+            textView3.text = "Nov 11 - 00:30 - Cuca Roseta"
+            textView4.text = "Nov 12 - 19:30 - Cuca Roseta"
+            textView5.text = "Nov 12 - 22:30 - Cuca Roseta"
+            textView6.text = "Nov 12 - 00:30 - Cuca Roseta"
+            textView7.text = "Nov 13 - 16:30 - Cuca Roseta"
+            textView8.text = "Nov 13 - 19:30 - Cuca Roseta"
+            textView9.text = "Nov 13 - 22:30 - Cuca Roseta"
+            textView10.text = "Palco 2"
+            textView11.text = "Nov 11 - 22:30 - Marco Paulo"
+            textView12.text = "Nov 11 - 00:30 - Marco Paulo"
+            textView13.text = "Nov 12 - 19:30 - Marco Paulo"
+            textView14.text = "Nov 12 - 22:30 - Marco Paulo"
+            textView15.text = "Nov 12 - 00:30 - Marco Paulo"
+            textView16.text = "Nov 13 - 16:30 - Marco Paulo"
+            textView17.text = "Nov 13 - 19:30 - Marco Paulo"
+            textView18.text = "Nov 13 - 22:30 - Marco Paulo"
         }
         return root
     }
--- FyreApp_NEW/app/src/main/java/com/example/fyreapp/ui/home/HomeViewModel.kt
+++ FyreApp_NEW/app/src/main/java/com/example/fyreapp/ui/home/HomeViewModel.kt
@@ -9,5 +9,9 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "Palco 1"
     }
+    private val _text2 = MutableLiveData<String>().apply {
+        value = "Nov 11 - 22:30 - Cuca Roseta"
+    }
     val text: LiveData<String> = _text
+    val text2: LiveData<String> = _text2
 }
